7a77c41b82fe96d205af0e777490b28896e7c723
unknown <23503669+amaster42@users.noreply.github.com>
2020-06-29 00:12:42 -0400

    powershell script to snatch wifi passwords in win10

---
+++ wifipass.ps1
@@ -0,0 +1,10 @@
+﻿$ssids = netsh wlan show profiles
+$ssid = ' '
+$delin = ': '
+
+#$ssid = $ssids.split(":")[1]
+$ssid = (echo $ssids | ForEach-Object {$_.split(":")[1]})
+#$ssid = (echo $ssid | ForEach-Object {$_.split(" ")})
+#echo $ssid | ForEach-Object -replace '\s+', ' '
+
+echo $ssid | Set-Content output.txt 
