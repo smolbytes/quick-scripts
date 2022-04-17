9591ea2f35421427227515303c7d1a8d7c7471b3
Alex Master <23503669+smolbytes@users.noreply.github.com>
2022-04-17 17:49:49 -0400

    extraneous step removed

--- kali-xrdp.sh
+++ kali-xrdp.sh
@@ -18,7 +18,7 @@ echo " "
 echo "[+] Starting xrdp service and enabling on boot"
 #echo "[+] Configuring xrdp to listen on port 3390 rather than default"
 #sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
-sudo echo 'xfce4-session' > ~/.xsession
+#sudo echo 'xfce4-session' > ~/.xsession
 echo 'startxfce4' | sudo tee -a /etc/xrdp/startwm.sh
 #sudo sed -i 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
 sudo systemctl enable xrdp-sesman #persist through reboots
